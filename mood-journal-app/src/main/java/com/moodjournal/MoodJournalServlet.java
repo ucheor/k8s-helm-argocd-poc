d6f56175b0e4dd9445d6b4889fb19f4c14fc133f
miriamora <miriamoranu@gmail.com>
2026-02-07 02:34:19 -0700

    replace mood-journal-app

---
+++ mood-journal-app/src/main/java/com/moodjournal/MoodJournalServlet.java
@@ -0,0 +1,19 @@
+package com.moodjournal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class MoodJournalServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        request.getRequestDispatcher("/WEB-INF/index.html").forward(request, response);
+    }
+}
